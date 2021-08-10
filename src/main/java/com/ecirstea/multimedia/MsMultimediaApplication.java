ed7a0b43d09124217c2b7b087bb592d40b7cfec5
elena <elender1230@gmail.com>
2021-08-10 18:51:18 +0200

    master: add eureka client

--- src/main/java/com/ecirstea/multimedia/MsMultimediaApplication.java
+++ src/main/java/com/ecirstea/multimedia/MsMultimediaApplication.java
@@ -2,8 +2,10 @@ package com.ecirstea.multimedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsMultimediaApplication {
 
 	public static void main(String[] args) {
